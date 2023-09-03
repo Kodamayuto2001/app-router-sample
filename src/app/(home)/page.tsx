ffc839bab0ef05ac570b2115b14356a4ed7d21a5
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2023-09-03 22:48:39 +0900

    add shadcn/ui

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
@@ -5,6 +6,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold text-center">Hello World</h1>
+      <Button>
+        押してください。
+      </Button>
     </main>
   )
 }
